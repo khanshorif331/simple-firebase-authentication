847ca3b3f4377d856dcc66239b86be6501bcb41d
khanshorif331 <khanshorif331@gmail.com>
2022-04-07 20:54:13 +0600

    google and github authentication setup done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,84 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
-  return <div className="App"></div>;
+  const [user, setUser] = useState({});
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error("error", error);
+      });
+  };
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
+
+  return (
+    <div className="App">
+      {user.uid ? (
+        <button onClick={handleSignOut}>Sign out</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Google sign in</button>
+          <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      )}
+
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+    </div>
+  );
 }
 
 export default App;
+
+// const provider = new GoogleAuthProvider();
+//   const handleGoogleSignIn = () => {
+//     signInWithPopup(auth, provider)
+//       .then((result) => {
+//         const user = result.user;
+//         console.log(user);
+//       })
+//       .catch((error) => {
+//         console.log("error", error);
+//       });
+//   };
--- src/_steps.js
+++ src/_steps.js
@@ -1,12 +1,15 @@
 /* 
 steps to firebase
 ** 1. create a projecat on console.firebase.google.com
-** 2. install firebase
+** 2. npm install firebase
 ** 3.register web app in firebase
 ** 4.copy firebase init with config from firebasse project settings into a file firebase.init;js
-**5.export default app
-6.iimport app firebase init.js into your app.js
-7.**import get auth from firebase/auth and create auth = getAuth(app)
-8.Tyrn on gooogle authentication
+**5.export default app from firebase.init.js
+6.**import get auth from firebase/auth and create const auth = getAuth(app) in App.js
+7.iimport app firebase init.js into your app.js 
+8.Turn on gooogle authentication (firebase > authentication > enable gooogle sing in)
+9.create google provider
+10.use signInWithPopus and pass auth and provider
+11. handle .then (if successful) and catch error (if error)
 
 */
